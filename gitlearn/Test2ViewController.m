317444a2d46ba9a86a9235f9acbff93e012aa9b8
jishu <bingfeng@ecook.cn>
2020-07-21 11:11:11 +0800

    ‘主分支修改’

---
+++ gitlearn/Test2ViewController.m
@@ -0,0 +1,32 @@
+//
+//  Test2ViewController.m
+//  gitlearn
+//
+//  Created by 技术 on 2020/7/21.
+//  Copyright © 2020 技术. All rights reserved.
+//
+
+#import "Test2ViewController.h"
+
+@interface Test2ViewController ()
+
+@end
+
+@implementation Test2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
